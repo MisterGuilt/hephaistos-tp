4d93498b472bed60fb361d821e18a854efb0db5f
MisterGuilt <37652522+MisterGuilt@users.noreply.github.com>
2020-03-01 22:36:40 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import createRouter from './router'
+import vuetify from './plugins/vuetify'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+
+const state = {
+  user: {}
+}
+
+new Vue({
+  data: state,
+  router: createRouter(state),
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import axios from 'axios'
+
+Vue.use(VueRouter)
+
+function createRouter (state) {
+  async function beforeEnter (to, from, next) {
+    try {
+      const { data: user } = await axios.get('http://localhost:3000/api/v1/me')
+      state.user = user
+      next()
+    } catch (err) {
+      console.log('err', err)
+      next('/login') // redirect to login if user is not authenticated
+    }
+  }
+
+  const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      beforeEnter
+    }
+  ]
+
+  return new VueRouter({
+    routes
+  })
+}
+export default createRouter
